4c1582feb104042df31d9755e8bd13c32d4f0990
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2019-10-08 10:42:30 +0300

    3. Массивы и модели. [#174892]

---
+++ chapter002/src/main/java/pojo/Book.java
@@ -0,0 +1,27 @@
+package pojo;
+
+public class Book {
+    private String name;
+    private int pages;
+
+    Book(String name, int pages) {
+        this.name = name;
+        this.pages = pages;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+}
---
+++ chapter002/src/main/java/pojo/Library.java
@@ -0,0 +1,25 @@
+package pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book tolstoi = new Book("Война и мир", 2048);
+        Book martin = new Book("Clean code", 464);
+        Book chehov = new Book("Человек в футляре", 1024);
+        Book gogol = new Book("Мертвые души", 999);
+
+        Book[] books = {tolstoi, martin, chehov, gogol};
+
+        for (Book bk : books) {
+            System.out.println(bk.getName() + " - " + bk.getPages());
+        }
+
+        books[0] = books[3];
+        books[3] = tolstoi;
+
+        for (Book bk : books) {
+            if (bk.getName().equals("Clean code")) {
+                System.out.println(bk.getName() + " - " + bk.getPages());
+            }
+        }
+    }
+}
