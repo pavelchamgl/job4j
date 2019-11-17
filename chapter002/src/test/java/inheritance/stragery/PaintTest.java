1fd898582bc24b7721fe07a52610042abcb9e15a
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2019-11-17 23:33:47 +0300

    9.1 Рефакторинг теста @Before @After [#33568]

--- chapter002/src/test/java/inheritance/stragery/PaintTest.java
+++ chapter002/src/test/java/inheritance/stragery/PaintTest.java
@@ -1,20 +1,37 @@
 package inheritance.stragery;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class PaintTest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
-                new String(out.toByteArray()),
+                this.out.toString(),
                 is(
                         new StringBuilder()
                                 .append("++++")
@@ -25,27 +42,22 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(
                 new String(out.toByteArray()),
                 is(
                         new StringBuilder()
-                            .append("   *")
-                            .append("  ***")
-                            .append(" *****")
-                            .append("*******")
-                            .append(System.lineSeparator())
-                            .toString()
+                                .append("   *")
+                                .append("  ***")
+                                .append(" *****")
+                                .append("*******")
+                                .append(System.lineSeparator())
+                                .toString()
                 )
         );
-        System.setOut(stdout);
     }
 }
