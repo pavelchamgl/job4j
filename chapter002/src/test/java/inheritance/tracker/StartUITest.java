5d4ba360dedeea81e61464ea603042990c88bf19
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2019-11-17 21:31:08 +0300

    9. Написать тесты на StartUI. [#785]

--- chapter002/src/test/java/inheritance/tracker/StartUITest.java
+++ chapter002/src/test/java/inheritance/tracker/StartUITest.java
@@ -1,44 +1,17 @@
 package inheritance.tracker;
 
-import org.hamcrest.core.IsNull;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
     @Test
-    public void whenAddItem() {
-        String[] answers = {"Pavel"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Pavel");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                item.getId(),
-                "replaced item"
-        };
-        StartUI.replaceItem(new StubInput(answers), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("replaced item"));
-    }
-    @Test
-    public void whenDeleteItem() {
-        String[] answers = {"Pavel"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item item = tracker.findAll()[0];
-        String[] answers2 = {item.getId()};
-        StartUI.deleteItem(new StubInput(answers2), tracker);
-        Item delete = tracker.findById(item.getId());
-        assertThat(delete, IsNull.nullValue());
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
     }
 }
