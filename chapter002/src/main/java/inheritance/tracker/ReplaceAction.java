8c523e1ebf22866ad24dbe5c53679cb77f6f6998
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2019-11-25 10:46:24 +0300

    1. Обеспечить бесперебойную работу приложения Tracker. [#789] -Добавлена проверка по id на наличие его в системе при замене заявки.

--- chapter002/src/main/java/inheritance/tracker/ReplaceAction.java
+++ chapter002/src/main/java/inheritance/tracker/ReplaceAction.java
@@ -8,11 +8,17 @@ public class ReplaceAction implements UserAction{
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
+        boolean result = false;
         String id = input.askStr("Enter Id: ");
         String name = input.askStr("Enter new name: ");
         Item item = new Item(name);
         item.setId(id);
-        tracker.replace(id, item);
-        return true;
+        if (tracker.replace(id, item)) {
+            System.out.println("Replace successfull");
+            result = true;
+        }else {
+            System.out.println("This ID not found");
+        }
+        return result;
     }
 }
